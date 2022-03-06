01037b7d3895769d49aaf06487645e7743deb6de
Brian <briancawley93@gmail.com>
2022-03-06 10:24:39 +0000

    Map array of objects and return titles only.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,28 @@
-const getTheTitles = function() {
+const getTheTitles = function(booksArray) {
+
+
+    
+    let titles = booksArray.map(book => book.title);
+    
+    return titles;
+
 
 };
+
+
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+
+
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
