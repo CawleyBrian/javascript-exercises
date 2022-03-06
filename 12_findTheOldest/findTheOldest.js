5c42e82b503bd9be2f17f51c75b1c1d5e376b81c
Brian <briancawley93@gmail.com>
2022-03-06 11:19:10 +0000

    reduce array of person objects return oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,29 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+
+    
+    let oldest = people.reduce((oldestPerson, person) => {
+        
+        if(person.yearOfDeath === undefined){
+            person.yearOfDeath = new Date().getFullYear();
+        }
+    
+        let age = person.yearOfDeath - person.yearOfBirth;
+
+        if(age > (oldestPerson.yearOfDeath - oldestPerson.yearOfBirth)){
+            oldestPerson = person;
+        }
+        return oldestPerson;
+        
+
+    },people[0]);
+
+    return oldest;
+    
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+
